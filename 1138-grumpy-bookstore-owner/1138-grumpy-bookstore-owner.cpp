7e34f4b8de991bc160a55033d69f90ef1e8d76a4
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2024-09-15 17:58:46 +0530

    Time: 28 ms (31.07%), Space: 34.7 MB (59.5%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -2,42 +2,31 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int n=customers.size();
-        int presum1=0;
-        int grusum=0;
-        int maxid=0;
+        int preloss=0;
         for(int i=0;i<minutes;i++){
-            if(grumpy[i]==1){
-                presum1+=customers[i];    
-            }
-            grusum+=grumpy[i];
+            if(grumpy[i]==1)preloss+=customers[i];
         }
-        int presummax=presum1;
-        int grusummax=grusum;
-        int i=1;int j=minutes;
+        int maxloss=preloss;
+        int maxId=0;
+        int i=1;
+        int j=minutes;
         while(j<n){
-            if(grumpy[j]==1){
-                presum1=presum1+customers[j];
+            int currloss=preloss;
+            if(grumpy[j]==1)currloss+=customers[j];
+            if(grumpy[i-1]==1)currloss-=customers[i-1];
+            if(currloss>maxloss){
+                maxloss=currloss;
+                maxId=i;
             }
-            if(grumpy[i-1]==1){
-                presum1=presum1-customers[i-1];
-            }
-            grusum=grusum+grumpy[j]-grumpy[i-1];
-            if(grusum>=grusummax && presum1>presummax){
-                    maxid=i;
-                    presummax=presum1;
-            }
-            // else if(grusum>grusummax){
-            //     maxid=i;
-            //     grusummax=grusum;
-            // }
+            preloss=currloss;
             i++;j++;
         }
-        // cout<<maxid<<endl;
-        int ans =0;
-        for (int i=0;i<n;i++){
-            if(i>=maxid && i<minutes+maxid)ans+=customers[i];
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            if(i>=maxId && i<minutes+maxId)ans+=customers[i];
             else if(grumpy[i]==0)ans+=customers[i];
         }
         return ans;
+
     }
 };
