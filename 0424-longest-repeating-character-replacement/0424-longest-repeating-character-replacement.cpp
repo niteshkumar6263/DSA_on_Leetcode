cf863bcddf63754685a84ec34aa7317d15478d68
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2024-10-20 12:25:06 +0530

    Time: 0 ms (100%), Space: 8.3 MB (92.67%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-     int characterReplacement(string s, int k) {
-        int e=2;
-            int maxi=0;
-            int l,r;
-        while(e--){
-            int n=s.size();
-             l=0,r=0;
-            int firstchange=0;
-            int maxchar=0;
-            vector<int>v(26);
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int maxchar=0;
+        int maxi=0;
+        int l=0,r=0;
+        vector<int>v(26);
+
             while(r<n){
-                v[(int)s[r]-65]++;
+                v[s[r]-65]++;
                 maxchar=max(maxchar,v[s[r]-65]);
-                // if(r-l+1-maxchar==1 && s[r]!=s[firstchange])firstchange=r;
-                for(int i=l+1;i<n;i++){
-                if(s[i]!=s[l]){
-                    firstchange=i;
-                    break;
-                }
-                }
-                // cout<<s[firstchange]<<" "<<firstchange<<" "<<r<<" "<<l<<endl;
                 if(r-l+1-maxchar>k){
+                    v[s[l]-65]--;
                     maxi=max(maxi,r-l);
-                    l=firstchange;
-                    maxchar=0;
-                    r=l-1;
-                    vector<int>y(26);
-                    v=y;
-                    y.clear();
-                    // cout<<"helo "<<l<<" "<<r<<endl;
+                    l++;
                 }
                 r++;
             }
-            reverse(s.begin(),s.end());
-        }
-        maxi=max(maxi,r-l);
-        return maxi;
+            maxi=max(r-l,maxi);
+            return maxi;   
     }
 };
