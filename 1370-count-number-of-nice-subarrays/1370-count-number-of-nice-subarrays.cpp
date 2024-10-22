2dac735dc19c5ee4c0cc5caad307ce1120f8145e
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2024-10-22 22:19:17 +0530

    Time: 13 ms (97.85%), Space: 70.4 MB (37.63%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-   int numberOfSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        int l=0,r=0,m=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2!=0){
-                l=i;
-                break;
-            }
+int countsub(vector<int>& nums, int k){
+    int count=0;
+    int n=nums.size();
+    int l=0,r=0;
+    int sum=0;
+    while(r<n){
+        sum+=nums[r]%2;
+        while(sum>k){
+            sum-=(nums[l]%2);
+            l++;
         }
-        r=l;
-        int countodd=0;
-        int count=0;
-        while(r<n){
-            if(nums[r]%2!=0){
-                countodd++;
-            }
-            if(countodd==k){
-                count+=l-m+1;
-            }
-            else if(countodd>k){
-                m=l+1;
-                countodd--;
-                for(int i=m;i<=r;i++){
-                    if(nums[i]%2!=0){
-                        l=i;
-                        break;
-                    }
-                }
-                count+=l-m+1;
-            }
-            r++;
+        if(sum<=k){
+            count+=r-l+1;
         }
-        return count;
+        r++;
+    }
+    return count;
+}
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return countsub(nums,k)-countsub(nums,k-1);
     }
 };
