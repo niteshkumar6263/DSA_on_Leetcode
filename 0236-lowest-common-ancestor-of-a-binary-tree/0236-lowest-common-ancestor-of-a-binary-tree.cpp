37ef166d6b4937e4bf6e758590dace22f188b1f0
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2025-01-05 10:08:21 +0530

    Time: 12 ms (65.38%), Space: 17.5 MB (30.12%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,18 +7,23 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
-public: 
-    bool find(TreeNode* root, TreeNode* f){
+public:
+    bool check(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode* & ans){
         if(root==NULL)return false;
-        if(root->val == f->val)return true;
-        return find(root->left,f) || find(root->right,f);
+        bool leftr = check(root->left,p,q,ans);
+        bool rightr = check(root->right,p,q,ans);
+        if((leftr && rightr) || ((leftr || rightr) && (root==p || root==q))){
+            ans = root;
+            return false;
+        }
+        if(leftr || rightr || root==p || root==q)return true;
+        return false;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p || root ==q)return root;
-        else if(find(root->left,p) && find(root->right,q))return root;
-        else if(find(root->right,p) && find(root->left,q))return root;
-        else if(find(root->right,p) && find(root->right,q))return lowestCommonAncestor(root->right,p,q);
-        else return lowestCommonAncestor(root->left,p,q);
+        TreeNode* ans = NULL;
+        check(root,p,q,ans);
+        return ans;
     }
 };
