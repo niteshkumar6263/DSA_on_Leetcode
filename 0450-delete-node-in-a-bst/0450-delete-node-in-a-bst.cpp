49a8f63e8f759aa46abe770ada65557525315c28
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2025-01-07 00:26:47 +0530

    Time: 0 ms (100%), Space: 34.3 MB (18.39%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,63 +11,29 @@
  */
 class Solution {
 public:
-    // TreeNode* findpre(TreeNode* root){
-    //     if(root->left==NULL){
-    //         return NULL;
-    //     }
-    //     TreeNode* pre = root->left;
-    //     while(pre->right!=NULL)pre = pre->right;
-    //     return pre->val;
-    // }
-    TreeNode* findsuc(TreeNode* root){
-        if(root->right==NULL){
-            return NULL;
-        }
-        TreeNode* succ = root->right;
-        while(succ->left!=NULL)succ = succ->left;
-        return succ;
-    }
-    TreeNode* del(TreeNode* root,int key, bool& flag){
-        if(root==NULL)return NULL;
-        if(root->val < key){
-            TreeNode* temp =  del(root->right, key,flag);
-            if(flag){
-                root->right = temp;
-                flag  = false;
-            }
-        }
-        else if(root->val>key){
-            TreeNode* temp =  del(root->left,key,flag);
-            if(flag){
-                root->left = temp;
-                flag = false;
-            }
-        }
-        else{
-            if(root->left == NULL && root->right == NULL){
-                flag = true; 
-                return NULL;
-            }
-            else if(root->left == NULL){
-                flag = true; 
-                return root->right;
-            }
-            else if(root->right ==NULL){
-                flag = true; 
-                return root->left;
-            }
-            else{
-                TreeNode* suc = findsuc(root);
-                suc->left  =  root->left;
-                flag = true;
-                return root->right;
-            }
-        }
-        return root;
-    }
+TreeNode* iop(TreeNode* root){
+    TreeNode* pre = root->left;
+    while(pre->right)pre = pre->right;
+    return pre;
+}
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root==NULL)return NULL;
-        bool flag = false;
-        return del(root,key,flag);
+        if(root->val==key){
+            if(root->left==NULL && root->right ==NULL)return NULL;
+            else if(root->left==NULL)return root->right;
+            else if(root->right == NULL)return root->left;
+            else{
+                TreeNode* pred = iop(root);
+                root->left = deleteNode(root->left,pred->val);
+                root->val = pred->val;
+            }
+        }
+        else if(root->val < key){
+            root->right = deleteNode(root->right,key);
+        }
+        else{
+            root->left = deleteNode(root->left,key);
+        }
+        return root;
     }
 };
