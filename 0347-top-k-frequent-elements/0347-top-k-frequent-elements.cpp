9cdd7a433e6f9d48b7a169b67b721787b15c3000
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2025-01-12 13:39:16 +0530

    Time: 3 ms (58.34%), Space: 18 MB (41.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    struct descend{
-        bool operator()(const pair<int,int>&a, const pair<int,int>&b) const{
-            return a.second >= b.second;
-        }
-    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n  = nums.size();
         map<int,int>m;
         for(int i = 0 ; i<n ; i++){
             m[nums[i]]++;
         }
-        set<pair<int,int>,descend>s;
+        vector<pair<int,int>>v;
         for(auto it = m.begin();it!=m.end();it++){
-            s.insert({it->first,it->second});
+            v.push_back({it->second,it->first});
         }
-        int count = 0;
+        m.clear();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>p;
+        for(int i = 0;i<v.size();i++){
+            p.push(v[i]);
+            if(p.size()>k)p.pop();
+        }
         vector<int>ans;
-        for(auto & it:s){
-            ans.push_back(it.first);
-            count++;
-            if(count==k)break;
+        while(!p.empty()){
+            ans.push_back(p.top().second);
+            p.pop();
         }
         return ans;
     }
