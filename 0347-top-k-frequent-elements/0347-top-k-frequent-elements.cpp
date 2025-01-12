4e9f54c90b29368bf091d30eeede2e7c1f71ae75
niteshkumar6263 <148683984+niteshkumar6263@users.noreply.github.com>
2025-01-12 12:20:11 +0530

    Time: 3 ms (58.34%), Space: 18.3 MB (23.8%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     struct descend{
         bool operator()(const pair<int,int>&a, const pair<int,int>&b) const{
-            return a.first >= b.first;
+            return a.second >= b.second;
         }
     };
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -13,12 +13,12 @@ public:
         }
         set<pair<int,int>,descend>s;
         for(auto it = m.begin();it!=m.end();it++){
-            s.insert({it->second,it->first});
+            s.insert({it->first,it->second});
         }
         int count = 0;
         vector<int>ans;
         for(auto & it:s){
-            ans.push_back(it.second);
+            ans.push_back(it.first);
             count++;
             if(count==k)break;
         }
